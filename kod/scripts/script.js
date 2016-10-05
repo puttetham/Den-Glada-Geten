cc10509499c00c723e05f46170a6937b115be78e
Skruvkork <wille.nystrom@gmail.com>
2016-10-05 15:32:23 +0200

    Script

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,5 +1,15 @@
 window.onload = init();
 
 function init() {
-  
+
+    var images = document.getElementsByClassName('gallery-img');
+    var bigImg = document.getElementById('big-img');
+
+    for (var i = 0; i < images.length; i++) {
+        images[i].addEventlistener('click', function() {expandImg(this)} );
+    }
+
+  function expandImg(evt) {
+
+  }
 }
