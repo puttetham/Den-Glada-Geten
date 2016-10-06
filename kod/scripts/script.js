60126d8c177103a6c015229ff3735742b4ddf29b
Skruvkork <wille.nystrom@gmail.com>
2016-10-06 16:02:26 +0200

    Gallerifunktion - halvfärdig

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,18 +1,25 @@
-window.onload = init;
+// window.onload = init;
+//
+// function init() {
+//
+//     var images = document.getElementsByClassName('gallery-img');
+//     var bigImg = document.getElementById('big-img');
+//     var lightBox = document.getElementById('lightbox');
+//
+//     for (var i = 0; i < images.length; i++) {
+//         images[i].addEventlistener('click', function() {expandImg(this)} );
+//     }
+//
+//   function expandImg(evt) {
+//       alert('hej');
+//       lightBox.setAttribute('class', 'expanded');
+//       bigImg.setAttribute('src', evt.src);
+//   }
+// }
+$(function () {
+    $('.gallery-img').on('click', function() {
+        $('#big-img').attr('src', this.src);
+        $('#lightbox').css('background-color', 'rgba(0, 0, 0, 1)');
 
-function init() {
-
-    var images = document.getElementsByClassName('gallery-img');
-    var bigImg = document.getElementById('big-img');
-    var lightBox = document.getElementById('lightbox');
-
-    for (var i = 0; i < images.length; i++) {
-        images[i].addEventlistener('click', function() {expandImg(this)} );
-    }
-
-  function expandImg(evt) {
-      alert('hej');
-      lightBox.setAttribute('class', 'expanded');
-      bigImg.setAttribute('src', evt.src);
-  }
-}
+    });
+});
