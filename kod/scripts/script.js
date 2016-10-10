9f929b909f31c5db3225501b7227661b237d8f7f
Skruvkork <wille.nystrom@gmail.com>
2016-10-10 19:13:21 +0200

    ofärdig kod

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,28 +1,16 @@
-// window.onload = init;
-//
-// function init() {
-//
-//     var images = document.getElementsByClassName('gallery-img');
-//     var bigImg = document.getElementById('big-img');
-//     var lightBox = document.getElementById('lightbox');
-//
-//     for (var i = 0; i < images.length; i++) {
-//         images[i].addEventlistener('click', function() {expandImg(this)} );
-//     }
-//
-//   function expandImg(evt) {
-//       alert('hej');
-//       lightBox.setAttribute('class', 'expanded');
-//       bigImg.setAttribute('src', evt.src);
-//   }
-// }
 $(function () {
+    var index = "";
     $('.gallery-img').on('click', function() {
         $('#big-img').attr('src', this.src);
+        index = $(this).index();
         $('#lightbox').css('display', 'block');
     });
 
-    $('#lightbox').on('click', function() {
-        $(this).css('display', 'none');
+    // $('#lightbox').on('click', function() {
+    //     $(this).css('display', 'none');
+    // });
+
+    $('#r-arrow').on('click', function() {
+         $('#big-img').attr('src', $('.gallery-img'));
     });
 });
