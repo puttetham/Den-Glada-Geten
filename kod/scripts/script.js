bc927841f336a0a29b9e991f7c4ed0515ae7257f
Skruvkork <wille.nystrom@gmail.com>
2016-10-11 16:00:42 +0200

    11/10
    Öppna/stänga bild fungerar. Bläddring har en bugg när man når
    början/slutet av bilderna.

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,16 +1,50 @@
 $(function () {
-    var index = "";
+    var images = [];
+    var files = ['images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/ybw4mnke.bmp', 'images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/8dfqwfjw.bmp', 'images/gallery-images/snc18tji.bmp', 'images/gallery-images/ybw4mnke.bmp','images/gallery-images/3lq7goxv.bmp'];
+
+    for (var i = 0; i < files.length; i++) {
+        var image = new Image();
+        image.src = files[i];
+        image.className = 'gallery-img';
+        images.push(image);
+    }
+
+    for (var i = 0; i < images.length; i++) {
+        $('#gallery').append(images[i]);
+    }
+
+    var i = 0;
+
     $('.gallery-img').on('click', function() {
         $('#big-img').attr('src', this.src);
-        index = $(this).index();
+        i = $(this).index();
+        console.log(i);
         $('#lightbox').css('display', 'block');
     });
 
-    // $('#lightbox').on('click', function() {
-    //     $(this).css('display', 'none');
-    // });
+
 
     $('#r-arrow').on('click', function() {
-         $('#big-img').attr('src', $('.gallery-img'));
+        console.log(i);
+        if (i > images.length-1) {
+            i = 0;
+        } else {
+            i++;
+        }
+         $('#big-img').attr('src', images[i].src);
     });
+
+    $('#l-arrow').on('click', function() {
+        console.log(i);
+        if(i < 1) {
+            i = images.length -1;
+        } else {
+            i--;
+        }
+         $('#big-img').attr('src', images[i].src);
+    });
+
+    $('#close').on('click', function() {
+        $('#lightbox').css('display', 'none');
+    })
 });
