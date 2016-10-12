c378e76990b288744593a03fbe71d9cdf338f13c
Skruvkork <wille.nystrom@gmail.com>
2016-10-12 15:11:32 +0200

    Galleri v1.0
    I galleriet kan man expandera och stänga ner bilder samt bläddra mellan
    bilder med pilar på skärmen eller med piltangenterna.

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,50 +1,67 @@
 $(function () {
     var images = [];
-    var files = ['images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/ybw4mnke.bmp', 'images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/8dfqwfjw.bmp', 'images/gallery-images/snc18tji.bmp', 'images/gallery-images/ybw4mnke.bmp','images/gallery-images/3lq7goxv.bmp'];
+    var files = ['images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/ybw4mnke.bmp', 'images/gallery-images/8dfqwfjw.bmp', 'images/gallery-images/snc18tji.bmp'];
 
+// Lägger till befintliga bilder i variabeln images
     for (var i = 0; i < files.length; i++) {
         var image = new Image();
         image.src = files[i];
         image.className = 'gallery-img';
         images.push(image);
     }
-
+// Rendera samtliga bilder i images på HTML-sidan i sektionen gallery
     for (var i = 0; i < images.length; i++) {
         $('#gallery').append(images[i]);
     }
 
     var i = 0;
-
+// Expandera klickad bild på skärmen
     $('.gallery-img').on('click', function() {
-        $('#big-img').attr('src', this.src);
+        $('#big-img').prop('src', this.src);
         i = $(this).index();
-        console.log(i);
         $('#lightbox').css('display', 'block');
     });
 
 
-
-    $('#r-arrow').on('click', function() {
-        console.log(i);
-        if (i > images.length-1) {
-            i = 0;
-        } else {
-            i++;
-        }
-         $('#big-img').attr('src', images[i].src);
-    });
-
-    $('#l-arrow').on('click', function() {
-        console.log(i);
-        if(i < 1) {
+// Bläddra med pilarna på skärmen
+    $('.arrow').on('click', function() {
+        this.id == 'arrow-right' ? i++ : i--;
+        if(i < 0) {
             i = images.length -1;
-        } else {
+        }
+        if(i > images.length -1) {
+            i = 0;
+        }
+
+        $('#big-img').prop('src', images[i].src);
+    });
+// Bläddra med piltangenterna
+    $('body').keydown(function(e) {
+        if (e.keyCode == 37) {
             i--;
         }
-         $('#big-img').attr('src', images[i].src);
-    });
+        if (e.keyCode == 39) {
+            i++;
+        }
 
+        if(i < 0) {
+            i = images.length -1;
+        }
+        if(i > images.length -1) {
+            i = 0;
+        }
+        console.log(i);
+        $('#big-img').prop('src', images[i].src);
+    })
+// Stäng ner expanderad bild genom att klicka på krysset eller på lightbox-diven
     $('#close').on('click', function() {
         $('#lightbox').css('display', 'none');
-    })
+    });
+
+    $('#lightbox').on('click', function(e) {
+        if (e.target !== this) {
+            return;
+        }
+        $('#lightbox').css('display', 'none');
+    });
 });
