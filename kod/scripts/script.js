59640e532f17dd074a64431a4e8c310bb88c94dc
Skruvkork <wille.nystrom@gmail.com>
2016-10-19 13:46:31 +0200

    Fix-2
    Fixade bugg där bilder blev fel klass

--- kod/scripts/script.js
+++ kod/scripts/script.js
@@ -1,35 +1,15 @@
 $(document).ready(function () {
     var images = [];
     var files = ['images/gallery-images/3lq7goxv.bmp', 'images/gallery-images/ybw4mnke.bmp', 'images/gallery-images/98699_ca_object_representations_media_2636_large.jpg', 'images/gallery-images/cropped-P1000045small.jpg', 'images/gallery-images/8dfqwfjw.bmp', 'images/gallery-images/stor_vandrarhem_rum3_stor.png', 'images/gallery-images/snc18tji.bmp'];
-
-// Lägger till befintliga bilder i variabeln images
-    for (var i = 0; i < files.length; i++) {
-        var image = new Image();
-        image.src = files[i];
-        image.className = 'gallery-img';
-        images.push(image);
-    }
-    displayImages();
-// Rendera samtliga bilder i images på HTML-sidan i sektionen gallery
-    function displayImages() {
-        for (var i = 0; i < images.length; i++) {
-            var div = $('<div class="img-frame"></div>');
-            var img = images[i];
-            var imgClass = ((img.height/img.width) < 1) ? ' wide' : ' tall';
-            img.className += imgClass;
-            div.append(img);
-            $('#gallery').append(div);
-        }
-    }
-
     var i = 0;
+    renderGallery();
+
 // Expandera klickad bild på skärmen
     $('.gallery-img').on('click', function() {
         $('#big-img').prop('src', this.src);
         i = $(this).index();
         $('#lightbox').css('display', 'block');
     });
-
 
 // Bläddra med pilarna på skärmen
     $('.arrow').on('click', function() {
@@ -72,4 +52,31 @@ $(document).ready(function () {
         }
         $('#lightbox').css('display', 'none');
     });
+
+    //Bestäm om bilden är bredare än den är hög
+    function determineImgClass() {
+        var imgClass = ((this.height/this.width) < 1) ? ' wide' : ' tall';
+        console.log(this);
+        console.log(this.height/this.width);
+        return this.className += imgClass;
+    }
+
+    // Rendera  bilden på HTML-sidan i sektionen gallery
+    function displayImages(img) {
+            var div = $('<div class="img-frame"></div>');
+            div.append(img);
+            $('#gallery').append(div);
+            img.onload = determineImgClass;
+    }
+
+    // Lägger till befintliga bilder i variabeln images
+    function renderGallery() {
+        for (var i = 0; i < files.length; i++) {
+            var image = new Image();
+            image.src = files[i];
+            image.className = 'gallery-img';
+            images.push(image);
+            displayImages(image);
+        }
+    }
 });
