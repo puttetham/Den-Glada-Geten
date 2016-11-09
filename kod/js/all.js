cbf461f845c57f2beb346bd1624b8bd5d7f957dd
Skruvkork <wille.nystrom@gmail.com>
2016-11-09 13:02:04 +0100

    fix

--- kod/js/all.js
+++ kod/js/all.js
@@ -18,7 +18,7 @@ function initMap() {
 //menu slide
 $(document).ready(function(){
 
-	var menu = $('#mobile-slide-menu')
+	var menu = $('#mobile-slide-menu');
 
 	$('#menu-bar').click(function(event){
 		event.preventDefault();
@@ -38,6 +38,21 @@ $(document).ready(function(){
   //KALENDER
   $(".datepicker").datepicker({
     dateFormat: "yy-mm-dd",
-    minDate: 0
-  });
+    dayNames: ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'],
+    dayNamesMin: ['Sön', 'Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör', 'Sön'],
+    firstDay: 1,
+    monthNames: ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'],
+
+    });
+
+    $('#arrival-date').datepicker({
+        minDate: new Date(),
+        onSelect: function(date){
+            var selectedDate = new Date(date);
+            var msecsInADay = 86400000;
+            var endDate = new Date(selectedDate.getTime() + msecsInADay);
+
+            $("#depart-date").datepicker( "option", "minDate", endDate );
+        }
+    });
 });
