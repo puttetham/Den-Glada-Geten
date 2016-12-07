045fb52ed2f08d7d68c19259b28e7d36de55409c
Axel <Axel@LAPTOP-223P8CEG>
2016-12-07 14:14:30 +0100

    aktivitetsboknings-fix
    aktivitetsbokning färdigställd, anpassat responsiviteten

--- kod/reservation.php
+++ kod/reservation.php
@@ -16,8 +16,12 @@
         $roomtype = $_POST['roomtype'];
         $arrival_date = $_POST['arrival_date'];
         $depart_date = $_POST['depart_date'];
-        $activity = $_POST['aktivitet'];
-
+        if (empty($_POST['activity'])){
+        $activity_string = "";
+        } else {
+        $activity = $_POST['activity'];
+        $activity_string = implode(", ", $activity);
+        }
         // Get all rooms of the selected room type
         $query = "SELECT *
                 FROM rooms
@@ -51,7 +55,7 @@
             //If no conflicting reservations were found, go through with the reservation!
             if ($isvacant) {
                 $reservquery = "INSERT INTO reservations(room, first_name, last_name, email, reserved_from, reserved_to, activity)
-                VALUES ('$desiredroom', '$firstname', '$lastname', '$email', '$arrival_date', '$depart_date', '$activity[0]')";
+                VALUES ('$desiredroom', '$firstname', '$lastname', '$email', '$arrival_date', '$depart_date', '$activity_string')";
 
                 mysqli_query($db, $reservquery);
 
@@ -63,6 +67,5 @@
             }
         }
         echo $response;
-        echo $activity;
     }
 ?>
